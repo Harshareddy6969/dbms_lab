ff3a0f88ef77f5f32da604b1b251d0b3bee41d79
Sai Harshith Reddy <64796899+Harshareddy6969@users.noreply.github.com>
2021-10-19 00:00:17 +0530

    Add files via upload

--- lab_6/assign6.sql
+++ lab_6/assign6.sql
@@ -1,1 +1,9 @@
+1) select * from student CROSS JOIN marks_sem1 ON student.rollno=marks_sem1.rollno;
 
+2) select student.name,marks_sem1.sports from student CROSS JOIN marks_sem1 ON student.rollno=marks_sem1.rollno;
+
+3) select student.name,Marks_sem1.math,student.rollno from student CROSS JOIN marks_sem1 ON student.rollno=marks_sem1.rollno cross join campus on student.cid=campus.cid where law=1; 
+
+4)select * from student left join campus on student.cid=campus.cid;
+
+  select * from student right join campus on student.cid=campus.cid;
